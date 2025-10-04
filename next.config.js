53acfc157376cf60e3088421f3067932a2dfd505
Matt Amann <ms@amann.pw>
2025-10-04 13:55:10 -0500

    Fix Vercel deployment - simplify Next.js config

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
     domains: ['res.cloudinary.com'],
     formats: ['image/webp', 'image/avif'],
   },
-  webpack: (config) => {
-    // Add support for HEIC files
-    config.module.rules.push({
-      test: /\.(heic|heif)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/images/',
-          outputPath: 'static/images/',
-        },
-      },
-    })
-    return config
-  },
 }
 
 module.exports = nextConfig
